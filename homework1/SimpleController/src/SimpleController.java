1ac0eec02640fad8e27f92dc707edf635baed606
solertm <43125415+solertm@users.noreply.github.com>
2018-12-01 11:22:18 +0800

    Add files via upload

---
+++ homework1/SimpleController/src/SimpleController.java
@@ -0,0 +1,32 @@
+package sc.ustc.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SimpleController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		resp.setContentType("text/html;charset=UTF-8");
+		PrintWriter out=resp.getWriter();
+		out.write("<html>\n"
+					+"	<head>\n"+
+							"<title>SimpleController</title>"+
+						"</head>"+
+						"<body>欢迎使用SimpleController!</body>"+
+					"</html>");
+	}
+
+}
